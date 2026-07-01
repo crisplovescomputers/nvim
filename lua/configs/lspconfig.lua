8db0feb28b2c8c084c20e5026b586f2c7202b191
crisplovescomputers <crisplovespcs@proton.me>
2026-07-01 23:34:19 +0300

    add 'nil_ls': LSP for Nix files

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "clangd", "ts_ls" }
+local servers = { "html", "cssls", "clangd", "ts_ls", "nil_ls" }
 -- local servers = { "html", "cssls", "lua_ls", "clangd", "ts_ls" }
 vim.lsp.enable(servers)
 
